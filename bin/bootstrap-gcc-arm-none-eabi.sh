a5071f6229583d91e540f736861592332932e3fc
Erik Cokeley <erik.cokeley@ring.com>
2020-04-05 22:33:10 -0400

    A fully bootstrapped Headless Ubuntu VM with gcc-arm-none-eabi. Bin directory has a script to download from arm.com and install. The compiler binary directory is added to the System PATH environment variable.

---
+++ bin/bootstrap-gcc-arm-none-eabi.sh
@@ -0,0 +1,36 @@
+#!/bin/bash -ex
+
+if [ ${EUID} -ne 0 ]; then
+	echo "ERROR: Must execute ${0} with root promissions!"
+	exit 1
+fi
+
+DOWNLOAD_DIR=$(mktemp)
+INSTALL_ROOT_DIR=/usr/local/bin
+INSTALL_GCC_DIR=${INSTALL_ROOT_DIR}/gcc-arm-none-eabi-9-2019-q4-major
+
+ARTIFACT_FILE_NAME="gcc-arm-none-eabi-9-2019-q4-major-x86_64-linux.tar.bz2?revision=108bd959-44bd-4619-9c19-26187abf5225&la=en&hash=E788CE92E5DFD64B2A8C246BBA91A249CB8E2D2D"
+MD5_HASH="fe0029de4f4ec43cf7008944e34ff8cc"
+
+DOWNLOAD_URL="https://developer.arm.com/-/media/Files/downloads/gnu-rm/9-2019q4/${ARTIFACT_FILE_NAME}"
+
+ARTIFACT_PATH=${DOWNLOAD_DIR}/${ARTIFACT_FILE_NAME}
+
+wget ${DOWNLOAD_URL} --quiet --directory-prefix=${DOWNLOAD_DIR}
+ARTIFACT_HASH=$(md5sum ${ARTIFACT_PATH} | cut -d " " -f 1)
+
+if [ "${ARTIFACT_HASH}" != "${MD5_HASH}" ]; then
+	echo "WARNING: Artifact hash does not match source!"
+else
+	$(
+		cd ${INSTALL_ROOT_DIR} &&
+		tar xjf ${ARTIFACT_PATH}
+	)
+fi
+
+PROFILE_SCRIPT=/etc/profile.d/gcc-arm-none-eabi.sh
+echo "export PATH=${PATH}:${INSTALL_GCC_DIR}/bin" > ${PROFILE_SCRIPT}
+chmod +u ${PROFILE_SCRIPT}
+
+rm -rf ${DOWNLOAD_DIR}
+
